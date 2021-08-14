88fee2ff198573c49636af81b5ecdcbe7de09b58
xvoron <artemvoronin95@gmail.com>
2021-08-14 18:18:56 +0200

    Refactoring + update README.md

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # Shop Tracking
+
+# Install
+Install docker form official repository:
+- https://docs.docker.com/get-docker/
+
+# Run
+There are two options to run application using script `run.sh` or manually
+enter commands line-by-line.
+
+## Using `run.sh`
+```bash
+./run.sh ~/path/to/files/
+```
+## Manually
+
+Build docker images of Visual and Tracking components; create a virtual
+network to provide communication between them:
+```bash
+docker image build -t shop:visual visual/   # Build Visual component image
+docker image build -t shop:track track/     # Build Tracking component image
+
+docker network create shopnet   # Create a network to communication
+```
+
+Run images in the separate terminals; change **<~/path/to/data>**:
+```bash
+# terminal 1
+docker run --network=shopnet -v <~/path/to/data>:/usr/src/shopapp/data/ --rm --name \
+    shop_visual_container --user $(id -u):$(id -g) -it shop:visual
+
+# terminal 2
+docker run --network=shopnet -v <~/path/to/data>:/usr/src/shopapp/data/ --rm --name \
+    shop_track_container --user $(id -u):$(id -g) -it shop:track
+```
+
+# Output
+Output video file `shop_tracking_video.avi` is saved in
+**<~/path/to/files/>**.
+
+# Troubleshooting
+## root privileges of output video file
+```bash
+sudo chown $(whoami):$(whoami) shop_tracking_video.avi
+```
+
+# Contacts
+- artemvoronin95@gmail.com
--- run.sh
+++ run.sh
@@ -1,29 +1,20 @@
 #!/usr/bin/bash
 
 DATAPATH=/usr/src/shopapp/data/
+ERRMSG=$'[Error] Add a path to files in full path format.
+\tExample: $./run.sh ~/path/to/directory/'
+
 
 if [ -z $1 ]
 then
-    echo "[Error] Add a path to files"
+    echo "$ERRMSG"
 else
     echo "[Info] Path to files is $1"
-    docker image build -t shop:visual visual/ || \
-        sudo docker image build -t shop:visual visual/
-
-    docker image build -t shop:track track/ || \
-        sudo docker image build -t shop:track track/
-
-    docker network create shopnet || sudo docker network create shopnet || echo "[Info] Network Done"
-
+    docker image build -t shop:visual visual/
+    docker image build -t shop:track track/
+    docker network create shopnet || echo "[Info] Network Done"
+    docker run -d --network=shopnet -v $1:$DATAPATH --rm --name \
+        shop_visual_container --user $(id -u):$(id -g) -it shop:visual
     docker run --network=shopnet -v $1:$DATAPATH --rm --name \
-        shop_visual_container -t shop:visual || \
-    sudo docker run --network=shopnet -v $1:$DATAPATH --rm --name \
-        shop_visual_container -t shop:visual
-
-
-    docker run --network=shopnet -v $1:$DATAPATH --rm --name \
-        shop_track_container -t shop:track || \
-    sudo docker run --network=shopnet -v $1:$DATAPATH --rm --name \
-        shop_track_container -t shop:track
-
+        shop_track_container --user $(id -u):$(id -g) -it shop:track
 fi
